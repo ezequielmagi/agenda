9f9e0cde981b40c770fee778e63ad32abe436e64
ezequiel magi <ezequielmaggi@gmail.com>
2022-02-08 15:56:15 -0300

    instalado y configurado bootstrap

--- agenda/src/App.js
+++ agenda/src/App.js
@@ -1,4 +1,4 @@
-
+import Button from 'react-bootstrap/Button';
 import './App.css';
 
 function App() {
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Hello World!</h1>
+        <Button variant='primary'>Hola</Button>
       </header>
     </div>
   );
